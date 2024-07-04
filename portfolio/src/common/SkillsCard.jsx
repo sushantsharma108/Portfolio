1f234b8862ec55f75f915002347f3db89b2fb87c
Sushant Sharma <ssushants786@gmail.com>
2024-07-04 19:11:15 +0530

    raw data added to skills

---
+++ portfolio/src/common/SkillsCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../sections/Skills/SkillsStyles.module.css'
+
+const SkillsCard = ({ src, data }) => {
+    return (
+        data.map((item, index) => (
+            <span key={index} className={styles.skills} >
+                <img src={src}/>
+                <p>{item}</p>
+            </span>
+        ))
+    )
+}
+
+export default SkillsCard
--- portfolio/src/sections/Skills/Skills.jsx
+++ portfolio/src/sections/Skills/Skills.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import checkmarkLight from '../../assets/checkmark-light.svg'
 import checkmarkDark from '../../assets/checkmark-dark.svg'
+import styles from '../Skills/SkillsStyles.module.css'
+import SkillsCard from '../../common/SkillsCard'
 
 const Skills = () => {
+
+    const data = ['HTML', 'TailwindCSS','BootStrap', 'JavaScript','MongoDB', 'Express', 'React', 'NodeJs','Git','GitHub']
     return (
-        <section>
-            Skills
+        <section id='skills' className={styles.container}>
+            <h1>Skills</h1>
+            <SkillsCard src={checkmarkDark} data={data}/>
         </section>
     )
 }
