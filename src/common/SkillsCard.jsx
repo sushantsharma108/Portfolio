d7876b2c8fbf6bd74a3caade3586408c4ae1df96
Sushant Sharma <ssushants786@gmail.com>
2024-07-25 10:04:23 +0530

    added some other assets

--- src/common/SkillsCard.jsx
+++ src/common/SkillsCard.jsx
@@ -3,11 +3,11 @@ import styles from '../sections/Skills/SkillsStyles.module.css'
 
 const SkillsCard = ({ src, data }) => {
     return (
-        data.map((item, index) =>(
-            <span key={index} className={styles.skills} >
+        data.map((item, index) => (
+            <div id='skills' key={index} className={styles.skills} >
                 <img src={src} />
                 <p>{item}</p>
-            </span>
+            </div>
         ))
     )
 }
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import checkmarkLight from '../../assets/checkmark-light.svg'
-import checkmarkDark from '../../assets/checkmark-dark.svg'
-import styles from '../Skills/SkillsStyles.module.css'
-import SkillsCard from '../../common/SkillsCard'
-import { useTheme } from '../../common/ThemeContext';
+import React from "react";
+// import checkmarkLight from '../../assets/checkmark-light.svg'
+// import checkmarkDark from '../../assets/checkmark-dark.svg'
+import styles from "../Skills/SkillsStyles.module.css";
+import SkillsCard from "../../common/SkillsCard";
+import { useTheme } from "../../common/ThemeContext";
 
 const Skills = () => {
-    const { theme } = useTheme();
-    const checkmarkIcon = theme === 'light' ? checkmarkLight : checkmarkDark;
-    const data = ['HTML', 'TailwindCSS', 'BootStrap', 'JavaScript', 'MongoDB', 'Express', 'React', 'NodeJs', 'Git', 'GitHub']
-    return (
-        <section id='skills' className={styles.container}>
-            <h1>Skills</h1>
-            <SkillsCard src={checkmarkIcon} data={data} />
-        </section>
-    )
-}
+  const { theme } = useTheme();
+  // const checkmarkIcon = theme === 'light' ? checkmarkLight : checkmarkDark;
+  const data = [
+    "HTML",
+    "TailwindCSS",
+    "BootStrap",
+    "JavaScript",
+    "MongoDB",
+    "Express",
+    "React",
+    "NodeJs",
+    "Git",
+    "GitHub",
+  ];
+  const pics = [
+  ];
+  return (
+    <section id="skills" className={styles.container}>
+      <h1>Skills</h1>
+      <SkillsCard src={pics} data={data} />
+    </section>
+  );
+};
 
-export default Skills
-
+export default Skills;
