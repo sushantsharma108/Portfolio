986852a6b3eac2ff1194ce1b57480d8fb92a1922
Sushant Sharma <ssushants786@gmail.com>
2024-07-25 13:06:26 +0530

    projectCard.jsx updated

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import styles from '../sections/Projects/ProjectStyles.module.css'
+import React from "react";
+import styles from "../sections/Projects/ProjectStyles.module.css";
 const ProjectCard = (props) => {
     // const { src, link, h3, p } = values;
     return (
-        <a href={props.link} target='_blank'>
-            <img className={styles.hover} src={props.src} alt="" srcset="" />
-            <h3>{props.h3}</h3>
-            <p>{props.p}</p>
-        </a>
-    )
-}
+        <div>
+            <div id={styles.project}>
+                <a href={props.link} target="_blank">
+                    <img className={styles.hover} src={props.src} alt="" srcset="" />
+                    <h3>{props.h3}</h3>
+                    <p>{props.p}</p>
+                </a>
+            </div>
 
-export default ProjectCard
+            <div id={styles.git}>
+                <a href={props.link} target="_blank">
+                    <i id={styles.icon} class="ri-github-fill"></i>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCard;
