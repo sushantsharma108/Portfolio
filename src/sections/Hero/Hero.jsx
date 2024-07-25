5f6f90fa3c1c486701c8398dfc22f4a1c3232774
Sushant Sharma <ssushants786@gmail.com>
2024-07-25 17:53:49 +0530

    added some assets

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -2,19 +2,16 @@ import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
-import githubLight from "../../assets/github-light.svg";
-import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import cv from "../../assets/CV.pdf"
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import cv from "../../assets/cv.pdf"
 import { useTheme } from "../../common/ThemeContext";
 
 const Hero = () => {
   const { theme, toggleTheme } = useTheme();
   const themeIcon = theme === 'light' ? sun : moon;
-  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedInIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
@@ -37,7 +34,7 @@ const Hero = () => {
       {/* this div contains info about person */}
       <div className={styles.info}>
         <h1>Sushant Sharma</h1>
-        <h2>Frontend Developer</h2>
+        <h2>Software Developer</h2>
         <span>
 
           <a href="https://www.linkedin.com/in/sushantsharma06" target="_blank" rel="noopener noreferrer">
@@ -53,14 +50,6 @@ const Hero = () => {
               className={styles.icon}
               src={githubIcon}
               alt="github"
-            />
-          </a>
-
-          <a href="mailto:sushant@example.com" target="_blank" rel="noopener noreferrer">
-            <img
-              className={styles.icon}
-              src={twitterIcon}
-              alt="x-twitter"
             />
           </a>
 
