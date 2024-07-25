2f72a4925197a001a02ac0f7d521afaaf2a60aa2
Sushant Sharma <ssushants786@gmail.com>
2024-07-25 12:05:12 +0530

    skills component updated

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-// import checkmarkLight from '../../assets/checkmark-light.svg'
-// import checkmarkDark from '../../assets/checkmark-dark.svg'
+import html from "../../assets/html5.png";
+import css from "../../assets/css3.png";
+import tailwind from "../../assets/tailwind.png";
+import bootstrap from "../../assets/bootstrap.png";
+import javascript from "../../assets/javascript.png";
+import mongodb from "../../assets/mongodb.png";
+import express from "../../assets/express.png";
+import react from "../../assets/react.png";
+import nodejs from "../../assets/nodejs.png";
+import git from "../../assets/git.png";
+import github from "../../assets/github.png";
 import styles from "../Skills/SkillsStyles.module.css";
 import SkillsCard from "../../common/SkillsCard";
 import { useTheme } from "../../common/ThemeContext";
 
 const Skills = () => {
   const { theme } = useTheme();
-  // const checkmarkIcon = theme === 'light' ? checkmarkLight : checkmarkDark;
   const data = [
     "HTML",
+    "CSS",
     "TailwindCSS",
     "BootStrap",
     "JavaScript",
@@ -21,6 +30,72 @@ const Skills = () => {
     "GitHub",
   ];
   const pics = [
+    {
+      id: 0,
+      src: html,
+      alt: "html image",
+      link: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
+    },
+    {
+      id: 1,
+      src: css,
+      alt: "css image",
+      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    },
+    {
+      id: 2,
+      src: tailwind,
+      alt: "tailwind css image",
+      link: "https://tailwindcss.com/",
+    },
+    {
+      id: 3,
+      src: bootstrap,
+      alt: "bootstrap image",
+      link: "https://getbootstrap.com/",
+    },
+    {
+      id: 4,
+      src: javascript,
+      alt: "javascript image",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    {
+      id: 5,
+      src: mongodb,
+      alt: "mongodb image",
+      link: "https://www.mongodb.com/",
+    },
+    {
+      id: 6,
+      src: express,
+      alt: "express image",
+      link: "https://expressjs.com/",
+    },
+    {
+      id: 7,
+      src: react,
+      alt: "react image",
+      link: "https://react.dev/",
+    },
+    {
+      id: 8,
+      src: nodejs,
+      alt: "nodejs image",
+      link: "https://nodejs.org/en",
+    },
+    {
+      id: 9,
+      src: git,
+      alt: "git image",
+      link: "https://git-scm.com/",
+    },
+    {
+      id: 10,
+      src: github,
+      alt: "github image",
+      link: "https://github.com/",
+    },
   ];
   return (
     <section id="skills" className={styles.container}>
